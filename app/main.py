3936c392d329221ddf1f0342eb58b2497c99b8fd
smark-ARK <arkhangreat2@gmail.com>
2021-12-11 10:22:37 +0530

    editing cors in main.py

--- app/main.py
+++ app/main.py
@@ -3,11 +3,22 @@ from sqlalchemy import engine
 from . import models
 from .database import engine
 from .routers import users, posts, auth, votes
+from fastapi.middleware.cors import CORSMiddleware
+
 
 models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(posts.router)
